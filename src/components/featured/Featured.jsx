556078fe8aeb4f2bcf805f601044bde10900e6fc
sameer <sameer@gmail.com>
2023-08-24 10:36:05 +0530

    first commit

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,43 @@
+import "./featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/216475546.webp?k=b3803ba3bc1f5c3b481ac390a752a7a3787c58a2b5c5e2081ecdc1c81fc11cd0&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/255854447.webp?k=f9c9d47f18b3156fdcc5596749d137f35e3cf4962a8d3d37d69be0066ec9bfc3&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Austin</h1>
+          <h2>532 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o= "
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Reno</h1>
+          <h2>533 properties</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
---
+++ src/components/searchItem/SearchItem.jsx
@@ -0,0 +1,41 @@
+import "./searchItem.css";
+
+const SearchItem = () => {
+  return (
+    <div className="SearchItem">
+      <img
+        src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=33621634dada92e85921bb6f35dec74fb6123d58e193940033ed108b7fe9db1e&o="
+        alt=""
+        className="siImg"
+      />
+      <div className="siDesc">
+        <h1 className="siTitle">Tower Street Apartments</h1>
+        <span className="siDistance">500m from Airport</span>
+        <span className="siTaxiOp">Free Airport Taxi</span>
+        <span className="siSubtitle">
+          Studio Apartment with Air conditioning
+        </span>
+        <span className="siFeatures">
+          Entire studio • 1 bathroom • 22m² 1 large double bed
+        </span>
+        <span className="siCancelOp">FREE cancellation</span>
+        <span className="siCancelOpSubtitle ">
+          You can cancel later , so lock in this great price today!
+        </span>
+      </div>
+      <div className="siDetails">
+        <div className="siRating">
+          <span>Excellent</span>
+          <button>8.9</button>
+        </div>
+        <div className="siDetailTexts">
+          <span className="siPrice">₹2,10,000</span>
+          <span className="siTaxOp">Includes GST and fees.</span>
+          <button className="siCheckButton">See availability > </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchItem;
