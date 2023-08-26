87c9c3d20393951ab1791f3104075ef9c1ab7ab7
sameer <sameer@gmail.com>
2023-08-26 17:27:46 +0530

    first commit

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -1,12 +1,12 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import "./searchItem.css";
 
 const SearchItem = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleAvailability = () => {
-    navigate("/hotels/:id");
-  };
+  // const handleAvailability = () => {
+  //   navigate("/hotels/:id");
+  // };
   return (
     <div className="SearchItem">
       <img
@@ -37,9 +37,7 @@ const SearchItem = () => {
         <div className="siDetailTexts">
           <span className="siPrice">₹2,10,000</span>
           <span className="siTaxOp">Includes GST and fees.</span>
-          <button onClick={handleAvailability} className="siCheckButton">
-            See availability
-          </button>
+          <button className="siCheckButton">See availability</button>
         </div>
       </div>
     </div>
